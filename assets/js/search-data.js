4e2c55ae7cbcff14f27b548b318e5556a8691db3
jburz2001 <56214083+jburz2001@users.noreply.github.com>
2025-07-23 18:52:54 +0000

    Deploying to gh-pages from @ jburz2001/jburz2001.github.io@2b4249d73e71ff5155ffa87280086cfde40e9f38 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "",
